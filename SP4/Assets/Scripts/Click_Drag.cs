8e3cccb29e4a7889ac6d0005eb988a476796ebdb
nerdonrun <nerdonrun@gmail.com>
2015-02-17 10:21:06 +0800

    Kun - Test
    Moshi Moshi

---
+++ SP4/Assets/Scripts/Click_Drag.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click_Drag : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        Debug.Log(onObject(100));
+    }
+
+    GameObject onObject(float distance)
+    {
+        Vector3 objPos = gameObject.transform.position;
+        RaycastHit ray;
+        Vector3 target = objPos + Camera.main.transform.forward * distance;
+        if (Physics.Linecast(objPos, target, out ray))
+        {
+            return ray.collider.gameObject;
+        }
+        return null;
+    }
+}
---
+++ SP4/Assets/Scripts/Drag.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drag : MonoBehaviour {
+
+    Ray ray;
+    RaycastHit rayhit;
+
+    void OnMouseDrag()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out rayhit)) {
+            transform.position = new Vector3(rayhit.point.x, rayhit.point.y, transform.position.z);
+        }
+    }
+}
