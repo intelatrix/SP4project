d05eb04c633a243e045f4f00d670d0cac8b91c4e
Puckasaur <fuadbinmohd@gmail.com>
2015-02-18 09:10:25 +0800

    Fuad's FPS
    Integrated FPS

---
+++ SP4/Assets/Scripts/GunScript.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunScript : MonoBehaviour {
+	
+
+	public float targetXRot;
+	public float targetYRot;
+	public float targetXRotV;
+	public float targetYRotV;
+	public float rotSpeed;
+	public float holdX;
+	public float holdY;
+	public float holdZ;
+	public float holdDown;
+	public float holdDownV;
+	public float aimSpeed;
+	public float aimingTrue;
+	public float zoomAngle;
+	public float fireRate;
+	public float waitTillFire;
+	public GameObject bullet;
+	public GameObject spawnBullet;
+	public GameObject cameraObject;	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetButton ("Fire1")) {
+			
+			if(waitTillFire <= 0)
+			{
+				if(bullet){
+					Instantiate(bullet, spawnBullet.transform.position, spawnBullet.transform.rotation);
+				}
+				waitTillFire = 1;
+			}
+			
+		}
+		
+		waitTillFire -= Time.deltaTime * fireRate;
+
+		MouseLook.targetCamera = zoomAngle;
+		
+		if(Input.GetButton("Fire2"))
+		{
+			holdDown = Mathf.SmoothDamp(holdDown, 0, ref holdDownV, aimSpeed);
+			MouseLook.aimTrue = aimingTrue;
+		}
+		else
+		{
+			holdDown = Mathf.SmoothDamp(holdDown, 1, ref holdDownV, aimSpeed);
+			MouseLook.aimTrue = 1;
+		}
+		
+		transform.position = cameraObject.transform.position + (Quaternion.Euler (0, targetYRot, 0) * new Vector3 (holdDown * holdX, holdDown * holdY, holdDown * holdZ));
+		targetXRot = Mathf.SmoothDamp (targetXRot, MouseLook.xRot, ref targetXRotV, rotSpeed);	
+		targetYRot = Mathf.SmoothDamp (targetYRot, MouseLook.yRot, ref targetYRotV, rotSpeed);
+
+		transform.rotation = Quaternion.Euler (targetXRot, targetYRot, 0);
+
+		//float rotation = Input.GetAxis("Mouse X") * rotSpeed;
+
+		//transform.RotateAround (cameraObject.transform.position, Vector3.up, rotation);//rotation * Time.deltaTime);
+	}
+}
---
+++ SP4/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook : MonoBehaviour {
+
+	float lookSens = 5;
+	public static float xRot;
+	public static float yRot;
+	float cur_xRot;
+	float cur_yRot;
+	float xRotV;
+	float yRotV;
+	float lookSmoothness = 0.1f;
+
+	float heightRatio = 0.9f;
+	public static float aimTrue = 1;
+	float cameraDefault = 60;
+	public static float targetCamera = 60;
+	float cameraZoom = 1;
+	float cameraZoomV;
+	float cameraZoomSpeed;
+
+	void Update ()
+	{
+		if (aimTrue == 1) 
+		{
+			cameraZoom = Mathf.SmoothDamp(cameraZoom, 1, ref cameraZoomV, cameraZoomSpeed);
+		}
+		else
+		{
+			cameraZoom = Mathf.SmoothDamp(cameraZoom, 0, ref cameraZoomV, cameraZoomSpeed);
+		}
+
+		camera.fieldOfView = Mathf.Lerp (targetCamera, cameraDefault, cameraZoom);
+
+		yRot += Input.GetAxis ("Mouse X") * lookSens;
+		xRot -= Input.GetAxis ("Mouse Y") * lookSens;
+
+		xRot = Mathf.Clamp(xRot, -80, 100);
+
+		cur_xRot = Mathf.SmoothDamp (cur_xRot, xRot, ref xRotV, lookSmoothness);
+		cur_yRot = Mathf.SmoothDamp (cur_yRot, yRot, ref yRotV, lookSmoothness);
+
+		transform.rotation = Quaternion.Euler (xRot, yRot, 0);
+	}
+
+ 
+}
