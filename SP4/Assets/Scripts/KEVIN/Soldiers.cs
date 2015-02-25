7296d4c7c931c0bbd175767f134f5790055bcb10
kevintps <hahasb3@hotmail.com>
2015-02-25 10:46:30 +0800

    kvein
    scipts

---
+++ SP4/Assets/Scripts/KEVIN/Soldiers.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Soldiers : MonoBehaviour {
+	public float x = -15.2f;
+	public string strtoenter;
+	public int random;
+	public GameObject soldiers;
+	public bool finish;
+	// Use this for initialization
+	void Start () {
+		finish = false;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//print (random);
+
+		if (random == 5) {
+			//random = 5;
+			if (finish == false) {
+				if (x < 15.0f) {
+					x += 0.1f;
+					//random = 0;
+				}
+			}
+		} else
+			random = Random.Range (1, 100);	
+		if (Input.GetKeyDown ("q")) {
+			finish = true;
+			finish = false;
+			random = 0;
+			
+		}
+		transform.position = new Vector3 (x, -9.9f, 0);
+	}
+}
---
+++ SP4/Assets/Scripts/KEVIN/character.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class character : MonoBehaviour {
+	// Use this for initialization
+	public GameObject charater1;
+	public GameObject charater;
+	public GameObject charater2;
+	public GameObject charater3;
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown ("u"))
+		{	
+			if(GameObject.Find ("character(Clone)"))
+			{
+				Instantiate (charater1, new Vector3 (18.8f, -10.9f, 0), Quaternion.identity) ;
+				GameObject.Destroy (GameObject.Find ("character(Clone)"));
+
+			}
+			else if(GameObject.Find ("character2(Clone)"))
+			{
+				Instantiate (charater3, new Vector3 (22.1f, -10.9f, 0), Quaternion.identity) ;
+				GameObject.Destroy (GameObject.Find ("character2(Clone)"));
+				
+			}
+
+		}
+		else if (Input.GetKeyDown ("p")) 
+		{
+			if(GameObject.Find ("character1(Clone)"))
+			{
+				Instantiate (charater, new Vector3 (18.8f, -10.9f, 0), Quaternion.identity) ;
+				GameObject.Destroy (GameObject.Find ("character1(Clone)"));
+			}
+			else if(GameObject.Find ("character3(Clone)"))
+			{
+				Instantiate (charater2, new Vector3 (22.1f, -10.9f, 0), Quaternion.identity) ;
+				GameObject.Destroy (GameObject.Find ("character3(Clone)"));
+			}
+		}
+	
+	
+
+		if (Input.GetKeyDown ("k"))
+		{
+			if(GameObject.Find ("character1(Clone)"))
+			{
+				GameObject.Destroy (GameObject.Find ("character1(Clone)"));
+				Instantiate (charater2, new Vector3 (22.1f, -10.9f, 0), Quaternion.identity) ;
+			}
+			else if(GameObject.Find ("character(Clone)"))
+			{
+				GameObject.Destroy (GameObject.Find ("character(Clone)"));
+				Instantiate (charater3, new Vector3 (22.1f, -10.9f, 0), Quaternion.identity) ;
+			}
+
+		}
+			
+	
+		else if (Input.GetKeyDown ("l"))
+		{
+			if(GameObject.Find ("character2(Clone)"))
+			{
+				GameObject.Destroy (GameObject.Find ("character2(Clone)"));
+				Instantiate (charater, new Vector3 (18.8f, -10.9f, 0), Quaternion.identity) ;
+			}
+			else if(GameObject.Find ("character3(Clone)"))
+			{
+				GameObject.Destroy (GameObject.Find ("character3(Clone)"));
+				Instantiate (charater1, new Vector3 (18.8f, -10.9f, 0), Quaternion.identity) ;
+			}
+		}
+
+}
+}
