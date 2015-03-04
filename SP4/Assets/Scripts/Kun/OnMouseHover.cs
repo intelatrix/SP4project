7af446caa5c893323dd0d6b64cb3eebd93cb4b9d
nerdonrun <nerdonrun@gmail.com>
2015-03-04 19:09:11 +0800

    FUCK SIA
    FUCK SIA

--- SP4/Assets/Scripts/Kun/OnMouseHover.cs
+++ SP4/Assets/Scripts/Kun/OnMouseHover.cs
@@ -19,12 +19,12 @@ public class OnMouseHover : MonoBehaviour {
 			content2.GetComponent<Text> ().text = "Pioneer Discount: " + GameObject.Find ("elderly").GetComponent<ElderlySetting> ().eventDiscount [1].ToString () + "% off";
 		} else if (name == "shopping_mart") {
 			title.GetComponent<Text> ().text = "Mart";
-			content.GetComponent<Text> ().text = "Cost: SGD$" + GameObject.Find ("elderly").GetComponent<ElderlySetting> ().costOfEvent [1].ToString ();
-			content2.GetComponent<Text> ().text = "Pioneer Discount: " + GameObject.Find ("elderly").GetComponent<ElderlySetting> ().eventDiscount [1].ToString () + "% off";
+			content.GetComponent<Text> ().text = "Cost: SGD$" + GameObject.Find ("elderly").GetComponent<ElderlySetting> ().costOfEvent [2].ToString ();
+			content2.GetComponent<Text> ().text = "Pioneer Discount: " + GameObject.Find ("elderly").GetComponent<ElderlySetting> ().eventDiscount [2].ToString () + "% off";
 		} else if (name == "church") {
 			title.GetComponent<Text> ().text = "Church";
-			content.GetComponent<Text> ().text = "Cost: SGD$" + GameObject.Find ("elderly").GetComponent<ElderlySetting> ().costOfEvent [1].ToString ();
-			content2.GetComponent<Text> ().text = "Pioneer Discount: " + GameObject.Find ("elderly").GetComponent<ElderlySetting> ().eventDiscount [1].ToString () + "% off";
+			content.GetComponent<Text> ().text = "Cost: SGD$" + GameObject.Find ("elderly").GetComponent<ElderlySetting> ().costOfEvent [3].ToString ();
+			content2.GetComponent<Text> ().text = "Pioneer Discount: " + GameObject.Find ("elderly").GetComponent<ElderlySetting> ().eventDiscount [3].ToString () + "% off";
 		}   else if (name == "Car") {
 			title.GetComponent<Text> ().text = "How to Cheat?";
 			content.GetComponent<Text> ().text = "Actual Cost: SGD$" + GameObject.Find ("elderly").GetComponent<ElderlySetting> ().actualExpense.ToString ();
